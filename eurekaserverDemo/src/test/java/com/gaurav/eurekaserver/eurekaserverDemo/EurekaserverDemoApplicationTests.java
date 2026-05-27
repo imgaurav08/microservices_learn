1eee8ce5e0167a87585c42f6c8e248acdee615c5
imgaurav08 <gmathur244@gmail.com>
2026-05-27 20:36:54 +0530

    Added Eureka Server Discovery

---
+++ eurekaserverDemo/src/test/java/com/gaurav/eurekaserver/eurekaserverDemo/EurekaserverDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaurav.eurekaserver.eurekaserverDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
