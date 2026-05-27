aa5b2c8c7bf7c126feaac5c237482006998650b7
imgaurav08 <gmathur244@gmail.com>
2026-05-27 23:49:11 +0530

    Added API Gateway

---
+++ APIGateway/src/test/java/com/gaurav/apiGateway/APIGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaurav.apiGateway.APIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
